d82f945753c64b58f388723f3cba43c06cfafdd7
Surya <thesuryapratap2003@gmail.com>
2024-06-02 18:20:58 +0530

    fixed a import statement

--- client/src/components/PatentForm.jsx
+++ client/src/components/PatentForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { backend_Uri } from '../config/constants';
-import styles from "../components/PatentForm.module.css";
+import styles from "./PatentForm.module.css";
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Motion from './Motion';
