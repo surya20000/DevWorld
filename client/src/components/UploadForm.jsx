e45af8dac5041d273a30cf3b049ac30569948bc2
Surya <thesuryapratap2003@gmail.com>
2024-06-02 18:11:36 +0530

    UI Changes

--- client/src/components/UploadForm.jsx
+++ client/src/components/UploadForm.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { backend_Uri } from "../config/constants";
-import { Link, useNavigate } from "react-router-dom";
-import "../components/Components.css";
-import "./PatentForm.Module.css";
-import Motion from "./Motion";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { backend_Uri } from '../config/constants';
+import { Link, useNavigate } from 'react-router-dom';
+import Motion from './Motion';
+import styles from "./UploadForm.module.css"
+import img1 from '../assets/3.png';
 
 const UploadForm = ({ getAllMedias }) => {
   const [email, setEmail] = useState("");
@@ -12,9 +12,9 @@ const UploadForm = ({ getAllMedias }) => {
   const [projectDescription, setProjectDescription] = useState("");
   const [deployedLink, setDeployedLink] = useState("");
   const [videos, setVideos] = useState([]);
-  const [successMsg, setSuccessMessage] = useState("");
-  const [errMsg, setErrMessage] = useState("");
-  const navigate = useNavigate();
+  const [successMsg, setSuccessMessage] = useState("")
+  const [errMsg, setErrMessage] = useState("")
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,23 +28,13 @@ const UploadForm = ({ getAllMedias }) => {
     for (let key in videos) {
       formData.append("videos", videos[key]);
     }
-    console.log(formData);
-    axios
-      .post(`${backend_Uri}/media/create`, formData)
-      .then((success) => {
-        console.log("Video Added successfully", success.message);
-        setSuccessMessage("Data Added Successfully,Get a Patent for yourself");
+    axios.post(`${backend_Uri}/media/create`, formData)
+      .then(() => {
+        setSuccessMessage("Data Added Successfully, Get a Patent for yourself");
       })
-      .catch((error) => {
-        console.error("Error", error.message);
-        if (
-          error.response &&
-          error.response.data &&
-          error.response.data.error
-        ) {
-          setErrMessage(
-            error.response.data.error.map((err) => err.message).join(", ")
-          );
+      .catch(error => {
+        if (error.response && error.response.data && error.response.data.error) {
+          setErrMessage(error.response.data.error.map(err => err.message).join(", "));
         } else {
           setErrMessage("Error adding info. Please try again.");
         }
@@ -59,16 +49,13 @@ const UploadForm = ({ getAllMedias }) => {
 
   useEffect(() => {
     const locallog = localStorage.getItem("logedIn");
-    console.log(locallog);
     if (locallog === "false") {
-      navigate("/login");
+      navigate('/login');
     }
-    axios
-      .get(`${backend_Uri}/media/developer/${localStorage.getItem("email")}`)
+    axios.get(`${backend_Uri}/media/developer/${localStorage.getItem("email")}`)
       .then((res) => {
-        console.log(("value", res.data.data));
         if (res.data.data === false) {
-          navigate("/");
+          navigate('/');
         }
       })
       .catch((err) => {
@@ -77,90 +64,45 @@ const UploadForm = ({ getAllMedias }) => {
   }, []);
 
   return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit}>
-        <div className="formGroup">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="formInput"
-            onChange={(e) => {
-              setEmail(e.target.value), console.log(email);
-            }}
-            placeholder="Enter your email"
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="Projectname">Project Name</label>
-          <input
-            type="text"
-            name="Projectname"
-            id="Projectname"
-            className="formInput"
-            onChange={(e) => {
-              setProjectName(e.target.value), console.log(projectName);
-            }}
-            placeholder="Capstone project name"
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="projectDescription">Project Description</label>
-          <input
-            type="text"
-            name="projectDescription"
-            id="projectDescription"
-            className="formInput"
-            onChange={(e) => {
-              setProjectDescription(e.target.value),
-                console.log(projectDescription);
-            }}
-            placeholder="Description of your project"
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="deployedLink">Share Deployed link</label>
-          <input
-            type="url"
-            name="deployedLink"
-            id="deployedLink"
-            className="formInput"
-            onChange={(e) => {
-              setDeployedLink(e.target.value), console.log(deployedLink);
-            }}
-            placeholder="http://abc@netlify.com"
-          />
-        </div>
-        <div className="formGroup">
-          <label htmlFor="videos">Upload Videos</label>
-          <input
-            type="file"
-            name="videos"
-            id="videos"
-            multiple
-            className="formInput"
-            accept=".mp4, .mkv"
-            onChange={(e) => {
-              setVideos(e.target.files), console.log(videos);
-            }}
-          />
-        </div>
-        <button type="submit">Submit</button>
-        <div className="endbtns">
-          <Link to="/displayMedias">
-            {" "}
-            <button className="workBtn"> Continue </button>{" "}
-          </Link>
-          <Link to="/patentForm">
-            {" "}
-            <button className="workBtn"> Get a Patent </button>{" "}
-          </Link>
-        </div>
-        {errMsg && <Motion text={errMsg} handleClose={handleClose} />}
-        {successMsg && <Motion text={successMsg} handleClose={handleClose} />}
-      </form>
+    <div className={styles.container}>
+      <div className={styles.Image}>
+           <img src={img1} alt="" />
+      </div>
+      <div className={styles.formcontainer}>
+         
+      
+        <form onSubmit={handleSubmit}>
+          <div className={styles.formInput}>
+            <label htmlFor="email">Email</label>
+            <input  name='email' id='email' className='formInput' onChange={(e) => { setEmail(e.target.value); }} placeholder='Enter your email' />
+          </div>
+          <div className={styles.formInput}>
+            <label htmlFor="Projectname">Project Name</label>
+            <input  name='Projectname' id='Projectname' className='formInput' onChange={(e) => { setProjectName(e.target.value); }} placeholder='Capstone project name' />
+          </div>
+          <div className={styles.formInput}>
+            <label htmlFor="projectDescription">Project Description</label>
+            <input  name='projectDescription' id='projectDescription' className='formInput' onChange={(e) => { setProjectDescription(e.target.value); }} placeholder='Description of your project' />
+          </div>
+          <div className={styles.formInput}>
+            <label htmlFor="deployedLink">Share Deployed link</label>
+            <input type="url" name='deployedLink' id='deployedLink' className='formInput' onChange={(e) => { setDeployedLink(e.target.value); }} placeholder='http://abc@netlify.com' />
+          </div>
+          <div className={styles.formInput}>
+            <label htmlFor="videos">Upload Videos</label>
+            <input type="file" name='videos' id='videos' multiple className='formInput' accept='.mp4, .mkv' onChange={(e) => { setVideos(e.target.files); }} />
+          </div>
+          <button>Submit</button>
+          <div className='endbtns'>
+            <Link to="/displayMedias"><button className='workBtn'>Continue</button></Link>
+            <Link to="/patentForm"><button className='workBtn'>Get a Patent</button></Link>
+          </div>
+          {errMsg && <Motion text={errMsg} handleClose={handleClose} />}
+          {successMsg && <Motion text={successMsg} handleClose={handleClose} />}
+        </form>
+      </div>
     </div>
+  
   );
 };
 
--- client/src/components/UploadsList.jsx
+++ client/src/components/UploadsList.jsx
@@ -1,11 +1,14 @@
-import { useState, useEffect } from "react";
-import { backend_Uri } from "../config/constants";
-import "../components/Components.css";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { motion, useMotionValue, useTransform, animate } from "framer-motion";
+import { useState, useEffect } from 'react';
+import { backend_Uri } from '../config/constants';
+import '../components/Components.css';
+import { Link, useLocation } from 'react-router-dom';
+import axios from 'axios';
+import { motion, useMotionValue, useTransform, animate } from 'framer-motion';
 import { useAuth0 } from "@auth0/auth0-react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import './Uploadlist.css';
 
 const UploadsList = ({ setSelected, item }) => {
   const [mediaWithDeveloperInfo, setMediaWithDeveloperInfo] = useState([]);
@@ -13,34 +16,28 @@ const UploadsList = ({ setSelected, item }) => {
   const count = useMotionValue(0);
   const rounded = useTransform(count, Math.round);
   const { user, getAccessTokenSilently } = useAuth0();
-  const [seachProject, setSearchProject] = useState("");
+  const [searchProject, setSearchProject] = useState("");
   const [list, setList] = useState([]);
   const navigate = useNavigate();
+  const location = useLocation();
+  const [showFullDescription, setShowFullDescription] = useState(false);
 
   useEffect(() => {
-    const res = mediaWithDeveloperInfo.filter(
-      (media) => media.projectName === seachProject
-    );
+    const res = mediaWithDeveloperInfo.filter(media => media.projectName.toLowerCase().includes(searchProject.toLowerCase()));
     setList([...res]);
-  }, [seachProject, mediaWithDeveloperInfo]);
+  }, [searchProject, mediaWithDeveloperInfo]);
 
   const tokenByauth0 = async () => {
     const token = await getAccessTokenSilently();
-    console.log("token by OAuth0", token);
-    console.log(user);
-    localStorage.setItem("jwt", token);
+    localStorage.setItem('jwt', token);
   };
 
   const googleSignIn = () => {
-    axios
-      .post(`${backend_Uri}/media/signInByGoogle`, {
-        name: user.name,
-        email: user.email,
-      })
-      .then((res) => {
+    axios.post(`${backend_Uri}/media/signInByGoogle`, { name: user.name, email: user.email })
+      .then(res => {
         console.log("Response", res.data);
       })
-      .catch((error) => console.log(error));
+      .catch(error => console.log(error));
   };
 
   useEffect(() => {
@@ -50,31 +47,29 @@ const UploadsList = ({ setSelected, item }) => {
       googleSignIn();
     }
     const locallog = localStorage.getItem("logedIn");
-    console.log(locallog);
     if (locallog === "false") {
-      navigate("/login");
+      navigate('/login');
     }
   }, []);
 
   const getAllMediaWithDeveloperInfo = async () => {
     try {
-      const response = await axios.get(
-        `${backend_Uri}/media/allWithDeveloperInfo`
-      );
+      const response = await axios.get(`${backend_Uri}/media/allWithDeveloperInfo`);
       setMediaWithDeveloperInfo(response.data);
-      const animation = animate(count, response.data.length, { duration: 1 });
-      return animation.stop;
+      animate(count, response.data.length, { duration: 1 });
     } catch (error) {
-      console.error("Error fetching media with developer info:", error);
+      console.error('Error fetching media with developer info:', error);
     }
   };
 
-  const filterProjects =
-    seachProject === ""
-      ? mediaWithDeveloperInfo
-      : mediaWithDeveloperInfo.filter(
-          (media) => media.projectName === seachProject
-        );
+  const handleSuggestionClick = (projectName) => {
+    setSearchProject(projectName);
+  };
+
+  // Function to toggle full description
+  const toggleDescription = () => {
+    setShowFullDescription(!showFullDescription);
+  };
 
   return (
     <motion.div
@@ -84,33 +79,32 @@ const UploadsList = ({ setSelected, item }) => {
       exit={{ opacity: 0 }}
       className="uploads-list-container"
     >
-      <div className="wrapper">
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search project by name"
-          onChange={(e) => setSearchProject(e.target.value)}
-        />
-        <motion.div layout className="suggestions">
-          {seachProject && (
-            <>
-              {mediaWithDeveloperInfo.map(
-                (media) =>
-                  media.projectName.includes(seachProject) && (
-                    <motion.div layout key={media.projectName} className="list">
+      <div className='Heading'>
+        <h1 className='explore'>
+          "Elevating Achievements, Celebrating Success: Your Path to Recognition Begins Here!"
+        </h1>
+        <p className='para'>Explore projects..Get Certificate for yourself..Be Own</p>
+        <div className="wrapper">
+          <input type="input" name="search" id="search" placeholder='Search project by name' value={searchProject} onChange={(e) => setSearchProject(e.target.value)} />
+          <motion.div layout className="suggestions">
+            {searchProject && (
+              <>
+                {mediaWithDeveloperInfo.map((media) => (
+                  media.projectName.toLowerCase().includes(searchProject.toLowerCase()) && (
+                    <motion.div layout key={media.projectName} className="list" onClick={() => handleSuggestionClick(media.projectName)}>
                       {media.projectName}
                     </motion.div>
                   )
-              )}
-            </>
-          )}
-        </motion.div>
+                ))}
+              </>
+            )}
+          </motion.div>
+        </div>
       </div>
-      <p className="contribution-statement">
+      <p className='contribution-statement'>
         An honor to present
         <motion.span
-          className="contribution-number"
+          className='contribution-number'
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
@@ -123,66 +117,63 @@ const UploadsList = ({ setSelected, item }) => {
         className="project-cards-container"
         initial={{
           opacity: 0,
-          y: 50,
+          y: 50
         }}
         whileInView={{
           opacity: 1,
           x: 0,
           y: 0,
           transition: {
-            duration: 1,
-          },
+            duration: 1
+          }
         }}
         viewport={{ once: true }}
         exit={{
           opacity: 0,
-          y: -50,
+          y: -50
         }}
       >
-        {filterProjects.map((media) => (
-          <div className="project-card" key={media._id}>
-            <h3 className="project-name">{media.projectName}</h3>
-            <div className="projDes">
-              <span> Description: </span>
-              <p className="project-description">{media.projectDescription}</p>
-            </div>
-            <div className="project-links">
-              <a
-                href={media.deployedLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="project-link"
-              >
-                View Project
-              </a>
-              {media.developerInfo ? (
-                <Link
-                  to={`/developer/${media.developerInfo.email}`}
-                  className="project-link"
-                >
-                  <span> Developer </span>
-                  {media.developerInfo.name}
-                </Link>
-              ) : (
-                <span className="not-available">
-                  Developer Info Not Available
-                </span>
-              )}
-              <Link to={`/editMedia/${media._id}`} className="action-link">
-                Edit
-              </Link>
-            </div>
-            <div className="videos-container">
-              {media.videos.map((video, index) => (
-                <div className="video" key={index}>
-                  <video className="uploaded-video" preload="auto" controls>
-                    <source
-                      src={`${backend_Uri}${video.replace(/\\/g, "/")}`}
-                    />
-                    Your browser does not support the video tag.
-                  </video>
+        {list.map(media => (
+          <div className="card" key={media._id}>
+            <Link to={`/editMedia/${media._id}`} className="action-link">
+              <FontAwesomeIcon icon={faEdit} />
+            </Link>
+            <div className='projectflex'>
+              <div className="project-info">
+                {media.developerInfo ? (
+                  <Link to={`/developer/${media.developerInfo.email}`} className="developerInfo">
+                    {media.developerInfo.name}
+                  </Link>
+                ) : (
+                  <span className="not-available">Developer Info Not Available</span>
+                )}
+              </div>
+              <div className="videos-container">
+                {media.videos.map((video, index) => (
+                  <div className="video" key={index}>
+                    <video className="image" preload='auto' controls>
+                      <source src={`${backend_Uri}${video.replace(/\\/g, '/')}`} />
+                      Your browser does not support the video tag.
+                    </video>
+                  </div>
+                ))}
+              </div>
+              <div className='card-info'>
+                <span>{media.projectName}</span>
+                <a href={media.deployedLink} target="_blank" rel="noopener noreferrer" className="project-link">
+                  View Project
+                </a>
+                <div>
+                  <h4 className="project-description">
+                    Description: {showFullDescription ? media.projectDescription : `${media.projectDescription.substring(0, 7)}...`}
+                    {media.projectDescription.length > 3 && (
+                      <h4 onClick={toggleDescription} className="readmore">
+                        {showFullDescription ? 'Read Less' : 'Read More'} <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                      </h4>
+                    )}
+                  </h4>
                 </div>
-              ))}
+              </div>
             </div>
           </div>
         ))}
@@ -190,10 +181,10 @@ const UploadsList = ({ setSelected, item }) => {
       <motion.button
         onClick={() => {
           logout();
-          localStorage.setItem("logedIn", false);
-          localStorage.setItem("signedIn", false);
+          localStorage.setItem('logedIn', false);
+          localStorage.setItem('signedIn', false);
         }}
-        className="logoutbtn"
+        className='logoutbtn'
         whileTap={{ scale: 0.85 }}
       >
         Logout
