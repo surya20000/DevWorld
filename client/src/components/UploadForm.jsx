6d71744eda2abb894244962280503184c40799a1
Surya <thesuryapratap2003@gmail.com>
2024-06-02 17:02:38 +0530

    Code added

--- client/src/components/UploadForm.jsx
+++ client/src/components/UploadForm.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { backend_Uri } from '../config/constants';
-import { Link, useNavigate } from 'react-router-dom';
-import '../components/Components.css'
-import './PatentForm.Module.css'
-import Motion from './Motion';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { backend_Uri } from "../config/constants";
+import { Link, useNavigate } from "react-router-dom";
+import "../components/Components.css";
+import "./PatentForm.Module.css";
+import Motion from "./Motion";
 
 const UploadForm = ({ getAllMedias }) => {
   const [email, setEmail] = useState("");
@@ -12,15 +12,15 @@ const UploadForm = ({ getAllMedias }) => {
   const [projectDescription, setProjectDescription] = useState("");
   const [deployedLink, setDeployedLink] = useState("");
   const [videos, setVideos] = useState([]);
-  const [successMsg, setSuccessMessage] = useState("")
-  const [errMsg, setErrMessage] = useState("")
-  const navigate = useNavigate()
+  const [successMsg, setSuccessMessage] = useState("");
+  const [errMsg, setErrMessage] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     let formData = new FormData();
-    formData.append("email", email)
+    formData.append("email", email);
     formData.append("projectName", projectName);
     formData.append("projectDescription", projectDescription);
     formData.append("deployedLink", deployedLink);
@@ -29,79 +29,136 @@ const UploadForm = ({ getAllMedias }) => {
       formData.append("videos", videos[key]);
     }
     console.log(formData);
-    axios.post(`${backend_Uri}/media/create`, formData)
-      .then(success => {
-        console.log('Video Added successfully', success.message);
-        setSuccessMessage("Data Added Successfully,Get a Patent for yourself")
+    axios
+      .post(`${backend_Uri}/media/create`, formData)
+      .then((success) => {
+        console.log("Video Added successfully", success.message);
+        setSuccessMessage("Data Added Successfully,Get a Patent for yourself");
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error", error.message);
-        if (error.response && error.response.data && error.response.data.error) {
-          setErrMessage(error.response.data.error.map(err => err.message).join(", "));
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.error
+        ) {
+          setErrMessage(
+            error.response.data.error.map((err) => err.message).join(", ")
+          );
         } else {
           setErrMessage("Error adding info. Please try again.");
         }
         setSuccessMessage("");
       });
-
   };
 
   const handleClose = () => {
     setSuccessMessage("");
-    setErrMessage("")
+    setErrMessage("");
   };
 
   useEffect(() => {
-    const locallog = localStorage.getItem("logedIn")
+    const locallog = localStorage.getItem("logedIn");
     console.log(locallog);
     if (locallog === "false") {
-      navigate('/login')
+      navigate("/login");
     }
-    axios.get(`${backend_Uri}/media/developer/${localStorage.getItem("email")}`)
+    axios
+      .get(`${backend_Uri}/media/developer/${localStorage.getItem("email")}`)
       .then((res) => {
-        console.log(("value" ,res.data.data));
-        if(res.data.data === false){
-          navigate('/')
+        console.log(("value", res.data.data));
+        if (res.data.data === false) {
+          navigate("/");
         }
       })
       .catch((err) => {
         console.log(err);
-      })
-
-  }, [])
-
+      });
+  }, []);
 
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit}>
-        <div className='formGroup'>
+        <div className="formGroup">
           <label htmlFor="email">Email</label>
-          <input type="email" name='email' id='email' className='formInput' onChange={(e) => { setEmail(e.target.value), console.log(email) }} placeholder='Enter your email' />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="formInput"
+            onChange={(e) => {
+              setEmail(e.target.value), console.log(email);
+            }}
+            placeholder="Enter your email"
+          />
         </div>
-        <div className='formGroup'>
+        <div className="formGroup">
           <label htmlFor="Projectname">Project Name</label>
-          <input type="text" name='Projectname' id='Projectname' className='formInput' onChange={(e) => { setProjectName(e.target.value), console.log(projectName) }} placeholder='Capstone project name' />
+          <input
+            type="text"
+            name="Projectname"
+            id="Projectname"
+            className="formInput"
+            onChange={(e) => {
+              setProjectName(e.target.value), console.log(projectName);
+            }}
+            placeholder="Capstone project name"
+          />
         </div>
-        <div className='formGroup'>
+        <div className="formGroup">
           <label htmlFor="projectDescription">Project Description</label>
-          <input type="text" name='projectDescription' id='projectDescription' className='formInput' onChange={(e) => { setProjectDescription(e.target.value), console.log(projectDescription) }} placeholder='Description of your project' />
+          <input
+            type="text"
+            name="projectDescription"
+            id="projectDescription"
+            className="formInput"
+            onChange={(e) => {
+              setProjectDescription(e.target.value),
+                console.log(projectDescription);
+            }}
+            placeholder="Description of your project"
+          />
         </div>
-        <div className='formGroup'>
+        <div className="formGroup">
           <label htmlFor="deployedLink">Share Deployed link</label>
-          <input type="url" name='deployedLink' id='deployedLink' className='formInput' onChange={(e) => { setDeployedLink(e.target.value), console.log(deployedLink) }} placeholder='http://abc@netlify.com' />
+          <input
+            type="url"
+            name="deployedLink"
+            id="deployedLink"
+            className="formInput"
+            onChange={(e) => {
+              setDeployedLink(e.target.value), console.log(deployedLink);
+            }}
+            placeholder="http://abc@netlify.com"
+          />
         </div>
-        <div className='formGroup'>
+        <div className="formGroup">
           <label htmlFor="videos">Upload Videos</label>
-          <input type="file" name='videos' id='videos' multiple className='formInput' accept='.mp4, .mkv' onChange={(e) => { setVideos(e.target.files), console.log(videos) }} />
+          <input
+            type="file"
+            name="videos"
+            id="videos"
+            multiple
+            className="formInput"
+            accept=".mp4, .mkv"
+            onChange={(e) => {
+              setVideos(e.target.files), console.log(videos);
+            }}
+          />
         </div>
-        <button type='submit'>Submit</button>
-        <div className='endbtns'>
-          <Link to="/displayMedias"> <button className='workBtn'> Continue </button> </Link>
-          <Link to="/patentForm"> <button className='workBtn'> Get a Patent </button> </Link>
+        <button type="submit">Submit</button>
+        <div className="endbtns">
+          <Link to="/displayMedias">
+            {" "}
+            <button className="workBtn"> Continue </button>{" "}
+          </Link>
+          <Link to="/patentForm">
+            {" "}
+            <button className="workBtn"> Get a Patent </button>{" "}
+          </Link>
         </div>
         {errMsg && <Motion text={errMsg} handleClose={handleClose} />}
         {successMsg && <Motion text={successMsg} handleClose={handleClose} />}
-
       </form>
     </div>
   );
